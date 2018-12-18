1eef0745a869be861754c4b693f2225ee87a569a
Salih <salihkulangara@gmail.com>
2018-12-18 10:28:01 +0530

    Update sk-wp-plugin.php

--- sk-wp-plugin.php
+++ sk-wp-plugin.php
@@ -2,7 +2,16 @@
 
 /*
 Plugin Name: SK Wordpress Plugin
+Plugin Description: This is a test plugin for checking the Private Updates
 Plugin Author: Salih K
 Plugin URI: https://salih.me
 Version: 1.0
 */
+
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/salihkulangara/sk-wp-plugin/',
+	__FILE__,
+	'sk-wp-plugin'
+);
